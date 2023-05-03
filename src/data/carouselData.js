3a1cd981059c7316fcd1a58a16739918150f6352
fraxbart <119609549+fraxbart@users.noreply.github.com>
2023-05-03 10:42:56 +0200

    Add files via upload

---
+++ src/data/carouselData.js
@@ -0,0 +1,20 @@
+export const carouselItems = [
+    {
+        id: 1,
+        title: 'Questo è il titolo della prima card',
+        description: 'Questa è la descrizione della prima card',
+        image: 'https://img.freepik.com/free-vector/night-landscape-with-lake-mountains-trees-coast-vector-cartoon-illustration-nature-scene-with-coniferous-forest-river-shore-rocks-moon-stars-dark-sky_107791-8253.jpg'
+    },
+    {
+        id: 2,
+        title: 'Questo è il titolo della seconda card',
+        description: 'Questa è la descrizione della seconda card',
+        image: 'https://wallpapercave.com/wp/wp6501320.jpg'
+    },
+    {
+        id: 3,
+        title: 'Questo è il titolo della terza card',
+        description: 'Questa è la descrizione della terza card',
+        image: 'https://i.pinimg.com/originals/3f/a0/40/3fa040b186dbfc221b96b62c7083c03b.jpg'
+    }
+]
