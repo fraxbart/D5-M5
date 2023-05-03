221b830a3ab8e93933f29648a682e104eb7654b3
fraxbart <119609549+fraxbart@users.noreply.github.com>
2023-05-03 10:40:40 +0200

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navigation from '../components/Navigation'
+import Main from '../components/Main'
+
+const Home = ({term}) => {
+  return (
+    <>
+      <Main term={term}/>
+    </>
+  )
+}
+
+export default Home
